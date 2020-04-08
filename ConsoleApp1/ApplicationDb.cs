959ebb8c8b0b522541b98bd4650891b3f344c76e
Live-PC\Live <live2345@gmail.com>
2020-04-08 14:16:57 +0430

    add EfCore to project

---
+++ ConsoleApp1/ApplicationDb.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleApp1
+{
+    public class ApplicationDb :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(LoggerFactory.Create(builder => 
+                    builder.AddConsole()))
+                .EnableSensitiveDataLogging()
+                .UseSqlServer(
+                    @"Server=(localdb)\MSSQLLocalDB;Database=EfCoredb;Integrated security = true;Trusted_Connection=True;ConnectRetryCount=0");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 
+
 namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (ApplicationDb db=new ApplicationDb())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+            }
             Console.WriteLine("Hello World!");
         }
     }
