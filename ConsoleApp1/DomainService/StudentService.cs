5cdf4ddd269bb872141685d51d89d983550ac285
Live-PC\Live <live2345@gmail.com>
2020-04-29 10:52:13 +0430

    Add Service Layer

---
+++ ConsoleApp1/DomainService/StudentService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+  
+
+  
+
+   
+    public  class StudentService
+    {
+        public  Student Read(int Id)
+        {
+            using (ApplicationDb db = new ApplicationDb())
+            {
+                Student stu= db.Students.Find(Id);
+                return stu;
+                Console.WriteLine(stu);
+            }
+        }
+        
+        private  void Delete(int id)
+        {
+            using (ApplicationDb db = new ApplicationDb())
+            {
+                Student stu= db.Students.Find(id);
+                db.Students.Remove(stu);
+                db.SaveChanges();
+            }
+        }
+        
+        private  void Update(int id, string newName, string newEmail)
+        {
+            using (ApplicationDb db = new ApplicationDb())
+            {
+                Student stu=db.Students.Find(id);
+                stu.Name = newName;
+                stu.Email = newEmail;
+                db.SaveChanges();
+            }
+        }
+        
+        public  void Create(string name, string email)
+        {
+            using (ApplicationDb db = new ApplicationDb())
+            {
+                Student student = new Student() {Name = name, Email = email};
+                db.Students.Add(student);
+                db.SaveChanges();
+
+            }
+
+        }
+    }
+
+
+}
